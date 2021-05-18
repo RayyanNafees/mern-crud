d1e14e5815fb20217da08b18c08e5275710ba113
RayyanNafees <nafees.rayyan@gmail.com>
2021-05-19 00:06:40 +0530

    listening

--- server/app.mjs
+++ server/app.mjs
@@ -11,8 +11,9 @@ app.get('/', (req, res)=> res.render('index.html'))
 app.get('/t', (req, res)=> res.send('index.html'))
 
 
-export default app;
-// app.listen(3000, ()=> {
-//     console.log('Hosting at http://localhost:3000/');
-//     open('http://localhost:3000/');
-// })
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('Express server listening on port', port)
+});
+
+// export default app;
