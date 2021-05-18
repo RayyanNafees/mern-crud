c6a98cae25a61d0a7107f239b0a112d50e77d8d1
RayyanNafees <nafees.rayyan@gmail.com>
2021-05-18 23:43:41 +0530

    trial /t

--- server/app.mjs
+++ server/app.mjs
@@ -6,6 +6,9 @@ app.use(_static('./client/build/'))
 
 app.get('/', (req, res)=> res.render('index.html'))
 
+app.get('/t', (req, res)=> res.send('index.html'))
+
+
 export default app;
 // app.listen(3000, ()=> {
 //     console.log('Hosting at http://localhost:3000/');
