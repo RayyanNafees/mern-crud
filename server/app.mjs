cbbc5f279fa1c8525caa74d787d859554a5cd806
RayyanNafees <nafees.rayyan@gmail.com>
2021-05-18 18:24:50 +0530

    Initial commit

---
+++ server/app.mjs
@@ -0,0 +1,12 @@
+import express, {static as _static} from 'express'
+import open from 'open';
+
+var app = express()
+app.use(_static('./client/build/'))
+
+app.get('/', (req, res)=> res.render('index.html'))
+
+app.listen(3000, ()=> {
+    console.log('Hosting at http://localhost:3000/');
+    open('http://localhost:3000/');
+})
